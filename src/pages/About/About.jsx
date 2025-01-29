34a70a0dd475942458534d2ce09aa72bbbde003e
IBoutajar <boutajar2b@hotmail.fr>
2025-01-29 15:53:19 +0100

    modif scss gallery and card

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,12 @@
 import './about.scss'
 import Header from '../../components/Header/Header'
+import Banner from '../../components/Banner/Banner'
 
 function About() {
     return (
         <div>
             <Header />
+            <Banner />
         </div>
     )
 }
