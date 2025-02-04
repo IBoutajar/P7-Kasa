ad1fe05e242ec98167290d1db421360c964b0979
IBoutajar <boutajar2b@hotmail.fr>
2025-02-04 16:03:30 +0100

    Page About w/ Compo Collapse

---
+++ src/components/Collapse/Collapse.jsx
@@ -0,0 +1,33 @@
+import './collapse.scss'
+import arrow from '../../assets/arrow.png'
+import { useState } from 'react'
+
+function Collapse({ title, content }) {
+    const [toggle, setToggle] = useState(false)
+
+    return (
+        <>
+            <div className="collapse">
+                <h3 onClick={() => setToggle(!toggle)}>
+                    <p>{title}</p>
+                    <img src={arrow} alt="show content" />
+                </h3>
+            </div>
+            <div
+                className={
+                    toggle ? 'collapse_content' : 'collapse_content_hidden'
+                }
+            >
+                {Array.isArray(content) ? (
+                    content.map((item, index) => {
+                        return <p key={index}>{item}</p>
+                    })
+                ) : (
+                    <p>{content}</p>
+                )}
+            </div>
+        </>
+    )
+}
+
+export default Collapse
